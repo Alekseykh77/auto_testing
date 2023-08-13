eea721ccb4724edd09296581bf5cbf6045c2f132
Alexey <alexh2@yandex.ru>
2023-08-13 11:55:12 +0300

    hw1

--- hw1/task2.py
+++ hw1/task2.py
@@ -1,8 +1,15 @@
+import subprocess
 import string
 
-s = "Hello, world! Hello, world!"
-print("The original string is : " + s)
+command = "cat /home/alexey/PycharmProjects/auto_testing/hw1/hello.txt "
 
-output = s.translate(str.maketrans('', '', string.punctuation))
-
-print(output)
+result = subprocess.run(command, shell=True, encoding="utf-8", stdout=subprocess.PIPE)
+result_out = result.stdout
+print(result_out)
+result_of_words = result_out.translate((str.maketrans("", "", string.punctuation)))
+print(result_of_words)
+if __name__ == '__main__':
+    if "Hello" in result_of_words and result.returncode == 0:
+        print("True")
+    else:
+        print("False")
